3dfe4f43fae889cdc129b938c857f6dc6a53dcc4
Ayush-398 <175736269+Ayush-398@users.noreply.github.com>
2025-11-08 13:40:30 +0530

    l

--- server/src/server.js
+++ server/src/server.js
@@ -1,34 +1,31 @@
 import express from 'express';
 import {ENV} from './lib/env.js';
 import Path from 'path';
+import { connectDB } from './lib/db.js';
+
 const app = express();
-
-
-
 const __dirname = Path.resolve();
 
-
-
-
-
-
-
-
-
 app.get('/',(req,res)=>{
-    res.status(200).send({message: "hello"})})
-
+    res.status(200).send({message: "hello"})
+});
 
 if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.use(express.static(Path.join(__dirname, "../frontend/dist"))); // Changed 'path' to 'Path'
+  app.get("/*", (req, res) => { // Changed "/{*any}" to "/*"
+    res.sendFile(Path.join(__dirname, "../frontend", "dist", "index.html")); 
   });
 }
 
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => 
+      console.log(`server is running on port ${ENV.PORT}`) 
+    );
+  } catch(error) {
+    console.error("Error starting the server!!", error);
+  }
+}
 
-
-app.listen(ENV.PORT,()=> 
- console.log(`server is running on port ${ENV.PORT}`));
-
+startServer(); 
