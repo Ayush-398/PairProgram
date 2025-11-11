a1cf50cc133cf134d9e3edc1eeda8144d34462d3
Ayush-398 <175736269+Ayush-398@users.noreply.github.com>
2025-11-11 23:49:39 +0530

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,9 @@ import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import { Toaster } from "react-hot-toast";
 import ProblemsPage from "./pages/ProblemsPage";
-
+import DashboardPage from "./pages/DashboardPage";
+//import ProblemPage from "./pages/ProblemPage";
+//import SessionPage from "./pages/SessionPage";
 
 function App() {
   const { isSignedIn, isLoaded } = useUser();
@@ -14,10 +16,9 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
-        <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
+       
         <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
-        <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
-        <Route path="/session/:id" element={isSignedIn ? <SessionPage /> : <Navigate to={"/"} />} />
+
       </Routes>
 
       <Toaster toastOptions={{ duration: 3000 }} />
