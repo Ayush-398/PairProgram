86604a2d76941a613ca74268eaf1ffa909bdab25
Ayush-398 <175736269+Ayush-398@users.noreply.github.com>
2025-11-12 23:02:49 +0530

    Fixed server.js

--- server/src/server.js
+++ server/src/server.js
@@ -35,7 +35,7 @@ app.get('/', requireAuth(), async (req, res) => {
 
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(Path.join(__dirname, "../frontend/dist"))); 
-app.use("/*", (req, res) => { 
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
 });
 }
